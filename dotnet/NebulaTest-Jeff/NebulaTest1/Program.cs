01ad6c8c45868c3f8e4bfa745d73e76f8ad395c1
Jeff Welsch <jeff@jeffwelsch.com>
2014-10-03 13:22:12 -0700

    Added .NET example

---
+++ dotnet/NebulaTest-Jeff/NebulaTest1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using net.openstack.Core.Providers;
+using net.openstack.Core.Exceptions.Response;
+using net.openstack.Core.Domain;
+
+namespace NebulaTest1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Program p = new Program();
+			p.Run3();
+
+		}
+		private void Run3()
+		{
+			CloudIdentityWithProject cloudIdentity = new CloudIdentityWithProject();
+			cloudIdentity.Username = "USERNAME";
+			cloudIdentity.Password = "PASSWORD";
+            cloudIdentity.ProjectName = "PROJECT NAME";
+            cloudIdentity.ProjectId = new ProjectId("PROJECT ID");
+
+			OpenStackIdentityProvider openStackIdentityProvider = new OpenStackIdentityProvider(new Uri("https://proxy.eagle.nebula.com:8770"));			
+			UserAccess userAccess = openStackIdentityProvider.Authenticate(cloudIdentity);
+            IdentityToken token = userAccess.Token;
+
+		}
+	}
+}
---
+++ dotnet/NebulaTest-Jeff/NebulaTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NebulaTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NebulaTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e2f7058-3ea0-4ca7-acf1-28d1f9b1ea5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
